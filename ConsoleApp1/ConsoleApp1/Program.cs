385536d1ec22b433cd49b716257a54e2177c3566
w68731 <w68731@RA117A-11>
2023-10-04 14:20:20 +0200

    kkkkk

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                Console.WriteLine("1.Sprawdz czy liczba jest parzysta i nie parzysta");
+                Console.WriteLine("2 Liczba parzysta");
+
+                var input = Console.ReadLine();
+
+                switch (let.Parse(input));
+                {
+                    case 1:
+                        DisplayNumberIsEven();
+                    break;
+                    case 2:
+                        break;
+                    case 0: return;
+
+                }
+
+
+            }
+
+        }
+
+        static void Game()
+        {
+            var random = new Random();
+            var number = Random.Next(1, 101,);//1-100
+
+            int count = 0;
+            int shoot;
+            do
+            {
+                count++;
+                Console.WriteLine("Podaj liczbe")
+                var input = Console.ReadLine();
+
+                shoot = int.Parse(input);
+            }
+            while (shoot 1=number);
+
+            Console.WriteLine($"Zgadłes za{count}");
+        }   
+
+
+        
+
+        static void DisplayNumberIsEven()
+        {
+            Console.WriteLine("Podaj liczbe:");
+            var input = Console.Readline();
+            if (int.Parse(input) % 2 == 0)
+            {
+                Console.WriteLine("Liczba jest parzytsa");
+            }
+            else
+            {
+                Console.WriteLine("liczab jest nieparzysta");
+            }
+
+        }
+
+
+                  
+         static bool NumberIsEven ()  
+
+                
+
+
+
+                
+
+
+
+                
+
+
+            
+        
+    }
+}
